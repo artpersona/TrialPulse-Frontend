0dc784d0bd9cf7a979a11306a353be6f82c36339
nonsense00027 <caneteneilfred@gmail.com>
2023-05-02 17:53:52 +0800

    added tabs on protocols page

--- src/pages/Protocols/ProtocolsMain/ProtocolsMain.jsx
+++ src/pages/Protocols/ProtocolsMain/ProtocolsMain.jsx
@@ -1,6 +1,7 @@
 import Study from "src/components/Protocols/Study";
 import "./ProtocolsMain.styles.css";
 import colorPalette from "src/utils/styles/colorPalette";
+import { useState } from "react";
 
 const getLetters = () => {
   let letters = [];
@@ -9,11 +10,54 @@ const getLetters = () => {
   }
   return letters;
 };
+
+const tabs = [
+  {
+    id: 0,
+    label: "ABC",
+  },
+  {
+    id: 1,
+    label: "Recently Viewed",
+  },
+  {
+    id: 2,
+    label: "Last Updated",
+  },
+];
+
 function ProtocolsMain() {
+  const [currentTab, setCurrentTab] = useState(0);
+
+  const isActive = (tab) => currentTab === tab;
+
+  function handleClick(tab) {
+    setCurrentTab(tab);
+  }
+
   return (
     <div className="protocols">
       {/* SIDEBAR */}
       <div className="protocols__sidebar">
+        {/* TOP BAR */}
+        <div
+          className="protocols__topBar"
+          style={{ backgroundColor: colorPalette.GRAY_DARK }}
+        >
+          <nav className="protocols__navbar">
+            <ul>
+              {tabs.map((item) => (
+                <li
+                  className={isActive(item.id) ? "active" : "inactive"}
+                  key={item.id}
+                  onClick={() => handleClick(item.id)}
+                >
+                  {item.label}
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </div>
         {/* STUDIES LIST */}
         <div className="protocols__sidebarLists">
           <Study title="Clinic Study" />
